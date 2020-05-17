dddf6ab7bc6cb3922adea9e0f2cd354c519d3c86
Neftali Hernandez <neftalih@usc.edu>
2020-05-17 01:58:42 -0700

    Ivans first commit test

---
+++ test_code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << argc << endl;
+
+	cout << "--------------" << endl;
+	cout << argv[0] << " "<< argv[1] << argv[2] << endl;
+
+	return 0;
+}
